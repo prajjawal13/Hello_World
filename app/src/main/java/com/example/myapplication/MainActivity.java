f226d6e914a653abd791f418a0b58fa7e591e7c6
Prajjawal gupta <prajjawalgupta13@gmail.com>
2021-01-17 12:21:16 +0530

    FirstACtivity will be added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity
 {
     TextView textView,textView1;
     Button button;
+    Context context;
+
      @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,14 +24,18 @@ public class MainActivity extends AppCompatActivity
         textView=findViewById(R.id.hello_world);
         textView1=findViewById(R.id.app_name);
         button=findViewById(R.id.button);
+
+        context=MainActivity.this;
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
                 Toast.makeText(MainActivity.this, "Click Me", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(context,FirstActivity.class);
+                startActivity(intent);
+
             }
         });
-
     }
 }
